c35d918690433aba383fd9fb0c8bec893ece5cfe
David Osorio <davidosoriorodriguez@gmail.com>
2022-05-09 02:36:03 +0200

    feat: created new route to display the epg view

--- src/routes/EpgScreen/EpgScreen.js
+++ src/routes/EpgScreen/EpgScreen.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Fragment, Suspense } from "react";
 
-import AppBar from '../../components/AppBar';
+import { fetchEPGScreenData } from "../../services/epg";
 
-import useStyles from './EpgScreen.style';
+import AppBar from "../../components/AppBar";
+import ProgrammingGrid from "../../components/EpgComponents/ProgrammingGrid";
+import Spinner from "./../../components/Spinner";
+
+import useStyles from "./EpgScreen.style";
+
+const resource = fetchEPGScreenData();
 
 const EpgScreen = () => {
   const classes = useStyles();
+  const { channels } = resource.read();
   return (
     <>
-    <AppBar />
-    <div className={classes.root}>
-      EPG Screen
-    </div>
+      <AppBar />
+      <div className={classes.root}>
+        <Suspense fallback={<Spinner />}>
+          <ProgrammingGrid channels={channels} />
+        </Suspense>
+      </div>
     </>
   );
 };
--- src/routes/EpgScreen/index.js
+++ src/routes/EpgScreen/index.js
@@ -1,1 +1,12 @@
-export { default } from './EpgScreen';
+import React, { Suspense } from "react";
+import Spinner from "../../components/Spinner";
+
+const EpgScreen = React.lazy(() => import("./EpgScreen"));
+
+const EpgScreenRoute = () => (
+  <Suspense fallback={<Spinner />}>
+    <EpgScreen />
+  </Suspense>
+);
+
+export default EpgScreenRoute;
