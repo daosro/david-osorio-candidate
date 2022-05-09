79495994609a1584a349398c18a565fe4a0ae29a
David Osorio <davidosoriorodriguez@gmail.com>
2022-05-09 02:32:02 +0200

    feat: add the main component of the grid

---
+++ src/components/EpgComponents/ProgrammingGrid/ProgrammingGrid.js
@@ -0,0 +1,58 @@
+import React, { useCallback, useRef } from "react";
+import { getListOfHoursInADay, getMinutesOfTheDay } from "../../../utils/date";
+import ChannelImage from "../ChannelImage";
+import ChannelSchedule from "../ChannelSchedule/ChannelSchedule";
+import TimeLine from "../TimeLine";
+
+import useStyles from "./ProgrammingGrid.style";
+
+const LIST_OF_HOURS_IN_A_DAY = getListOfHoursInADay();
+
+const ProgrammingGrid = ({ channels }) => {
+  const classes = useStyles();
+
+  const epgContainerRef = useRef(null);
+
+  const handleNowButtonClick = useCallback(() => {
+    const minutes = getMinutesOfTheDay();
+    const scrollToX =
+      (400 / 60) * minutes - (document.body.clientWidth / 2 - 50);
+    epgContainerRef.current.scrollTo(scrollToX, 0);
+  }, [epgContainerRef]);
+
+  return (
+    <>
+      <button className={classes.nowButton} onClick={handleNowButtonClick}>
+        NOW
+      </button>
+      <div className={classes.grid}>
+        <div ref={epgContainerRef} className={classes.gridProgramRow}>
+          <TimeLine numberOfChannels={channels.length} />
+          <div className={classes.hoursRow}>
+            >
+            {LIST_OF_HOURS_IN_A_DAY.map((hour) => (
+              <div key={hour} className={classes.hourRoot}>
+                <div className={classes.hour}>{hour} </div>
+                <div className={classes.hourMarker} />
+              </div>
+            ))}
+          </div>
+          {channels.map((channel, index) => (
+            <div key={channel.title} className={classes.channelRoot}>
+              <div className={classes.channelList}>
+                <ChannelImage
+                  key={channel.title}
+                  title={channel.title}
+                  logo={channel.images.logo}
+                />
+              </div>
+              <ChannelSchedule key={channel.title} channel={channel} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProgrammingGrid;
---
+++ src/components/EpgComponents/ProgrammingGrid/ProgrammingGrid.style.js
@@ -0,0 +1,67 @@
+import { createUseStyles } from "react-jss";
+
+export default createUseStyles(
+  {
+    root: {},
+    grid: {
+      display: "flex",
+      flexDirection: "row",
+      overflow: "hidden",
+    },
+    gridProgramRow: {
+      overflowX: "auto",
+      overflowY: "hidden",
+      position: "relative",
+    },
+    hoursRow: {
+      display: "flex",
+      flexDirection: "row",
+      marginLeft: "4rem",
+      width: "calc((400px * 24))",
+    },
+    hourRoot: {
+      width: "400px",
+      height: "2rem",
+      position: "relative",
+    },
+    hour: {
+      position: "absolute",
+      top: 0,
+      left: "-1.2rem",
+    },
+    hourMarker: {
+      position: "absolute",
+      bottom: 0,
+      left: "-1px",
+      height: "0.8rem",
+      borderLeft: "1px solid #393939",
+    },
+    channelRoot: {
+      display: "flex",
+      flexDirection: "row",
+      width: "calc((400px * 24) + 150px)",
+    },
+    channelList: {
+      position: "sticky",
+      left: 0,
+      zIndex: 3,
+      boxShadow: "7px 0 10px 0 #101010",
+    },
+    nowButton: {
+      position: "fixed",
+      height: "30px",
+      bottom: "65px",
+      right: "20px",
+      backgroundColor: "#e1a21e",
+      color: "#FFF",
+      borderRadius: "5px",
+      textAlign: "center",
+      boxShadow: "1px 1px 0px #939393",
+      zIndex: "4",
+      fontSize: "12px",
+      border: "1px solid #393939",
+      cursor: "pointer",
+    },
+  },
+  { name: "ProgrammingGrid" }
+);
