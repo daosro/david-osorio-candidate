01b16a1787ad76b8ff92738bde19e9dc39e57b0c
David Osorio <davidosoriorodriguez@gmail.com>
2022-05-10 14:46:26 +0200

    feat: Add styles for a simple AppBar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,20 +1,33 @@
-import React from "react";
-
-import { MdPerson, MdSearch } from "react-icons/md";
+import React, { useCallback } from "react";
+import clsx from "clsx";
+import { useNavigate  } from "react-router-dom";
+import { MdKeyboardArrowLeft, MdPerson, MdSearch } from "react-icons/md";
 
 import useStyles from "./AppBar.style";
 
-const AppBar = () => {
-  const classes = useStyles();
+const AppBar = ({ simple = false }) => {
+  const classes = useStyles({ simple });
+  const navigate  = useNavigate ();
+
+  const handleBack = useCallback(() => {
+    navigate(-1);
+  }, [navigate ]);
   return (
     <header className={classes.root}>
-      <MdPerson className={classes.userIcon} />
+      {simple ? (
+        <MdKeyboardArrowLeft
+          className={clsx(classes.icon, classes.marginLeft)}
+          onClick={handleBack}
+        />
+      ) : (
+        <MdPerson className={clsx(classes.icon, classes.marginLeft)} />
+      )}
       <img
         className={classes.logo}
         src={process.env.PUBLIC_URL + "/assets/images/norigin-media-logo.png"}
         alt="NoriginMedia"
       />
-      <MdSearch className={classes.searchIcon} />
+      <MdSearch className={clsx(classes.icon, classes.marginRight)} />
     </header>
   );
 };
--- src/components/AppBar/AppBar.style.js
+++ src/components/AppBar/AppBar.style.js
@@ -3,26 +3,29 @@ import { createUseStyles } from "react-jss";
 export default createUseStyles(
   {
     root: {
-      backgroundColor: "#000000",
+      backgroundColor: (props) => (props.simple ? "transparent" : "#000000"),
       display: "flex",
       width: "100%",
       height: "4rem",
       justifyContent: "space-between",
       alignItems: "center",
+      zIndex: 4,
+      position: "fixed",
     },
-    userIcon: {
-      marginLeft: "1rem",
+    icon: {
       height: "2em",
       width: "2em",
+      cursor: "pointer",
+    },
+    marginLeft: {
+      marginLeft: "1rem",
+    },
+    marginRight: {
+      marginRight: "1rem",
     },
     logo: {
       width: "3rem",
       height: "3rem",
-    },
-    searchIcon: {
-      marginRight: "1rem",
-      height: "2em",
-      width: "2em",
     },
   },
   { name: "AppBar" }
