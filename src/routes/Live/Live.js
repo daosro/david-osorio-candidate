1115943414f8364ef6a6c577f1a60807062e91ae
David Osorio <davidosoriorodriguez@gmail.com>
2022-05-10 19:05:02 +0200

    feat: add static video player in live route

--- src/routes/Live/Live.js
+++ src/routes/Live/Live.js
@@ -1,12 +1,49 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import AppBar from "../../components/AppBar";
+import VideoJS from "../../core/VideoJS/VideoJS";
 
 import useStyles from "./Live.style";
 
 const Live = () => {
   const classes = useStyles();
-  const { channelId } = useParams();
-  return <div className={classes.root}>Live channel {channelId}</div>;
+  // const { channelId } = useParams();
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: false,
+    fluid: true,
+    sources: [
+      {
+        src: "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      player.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      player.log("player will dispose");
+    });
+  };
+
+  return (
+    <>
+      <AppBar simple />
+      <div className={classes.player}>
+        <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      </div>
+    </>
+  );
 };
 
 export default Live;
--- src/routes/Live/Live.style.js
+++ src/routes/Live/Live.style.js
@@ -1,7 +1,18 @@
-import { createUseStyles } from 'react-jss';
+import { createUseStyles } from "react-jss";
 
-export default createUseStyles({
-  root: {
-
+export default createUseStyles(
+  {
+    root: {},
+    player: {
+      width: "67vw",
+      height: "0",
+      top: "4.1rem",
+      position: "relative",
+      alignItems: "center",
+      justifyContent: "center",
+      left: "50%",
+      transform: "translateX(-50%)",
+    },
   },
-}, { name: 'Live' });
+  { name: "Live" }
+);
