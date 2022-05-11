2108cf74411c33a31da07489cb53f12727c90c2a
David Osorio <davidosoriorodriguez@gmail.com>
2022-05-11 23:40:37 +0200

    feat: change README and Home page

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import AppBar from './../../components/AppBar';
+import AppBar from "./../../components/AppBar";
 
-import useStyles from './Home.style';
+import useStyles from "./Home.style";
 
 const Home = () => {
   const classes = useStyles();
   return (
     <>
-    <AppBar />
-    <div className={classes.root}>
-      Home
-    </div>
+      <AppBar />
+      <div className={classes.root}>
+        <h1> David Osorio Rodríguez </h1>
+        <p>
+          This Repo is intended to instruct new Frontend developer (Native &
+          Web) candidates on the steps to follow for completing the Norigin
+          Media testing task.
+        </p>
+        <h2>Please go to the epg section for a interactive experience</h2>
+        <img
+          className={classes.arrow}
+          src={process.env.PUBLIC_URL + "/assets/images/arrow.png"}
+          alt="NoriginMedia"
+        />
+      </div>
     </>
   );
 };
--- src/routes/Home/Home.style.js
+++ src/routes/Home/Home.style.js
@@ -1,7 +1,23 @@
-import { createUseStyles } from 'react-jss';
+import { createUseStyles } from "react-jss";
 
-export default createUseStyles({
-  root: {
-
+export default createUseStyles(
+  {
+    root: {
+      padding: "5rem 4.1rem 0",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+    },
+    arrow: {
+      left: '50%',
+      position: 'absolute',
+      transform: 'translateX(-90%)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      bottom: '10vh',
+      width: '12rem',
+    },
   },
-}, { name: 'Home' });
+  { name: "Home" }
+);
